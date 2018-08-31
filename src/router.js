a2ca8daecd4ed61ccd603140d46b444341d938f6
Isaiah White <isaiahwhite208@gmail.com>
2018-08-31 11:54:44 -0600

    push

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Game from './views/Game.vue'
 
 Vue.use(Router)
 
@@ -14,10 +15,7 @@ export default new Router({
     {
       path: '/game',
       name: 'game',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('./views/Game.vue')
+      component: Game
     }
   ]
 })
