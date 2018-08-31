89d8ba7b606a062b85e70cf7a20558dfaa5e5c19
Isaiah White <isaiahwhite208@gmail.com>
2018-08-30 20:53:07 -0600

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="home">
+    <MainGame msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
+  import MainGame from '@/components/MainGame.vue'
 
   export default {
     name: 'home',
+    components: {
+      MainGame
+    }
   }
 </script>
